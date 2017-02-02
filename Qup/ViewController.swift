0c9fd53daccbe8877d026709b682c1d2e41ba254
Tobias Lundblad <tobias.lundblad@Administrators-MacBook-Pro-14.local>
2017-02-02 10:21:31 +0100

    initial commit

---
+++ Qup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Qup
+//
+//  Created by Tobias Lundblad on 2017-01-18.
+//  Copyright © 2017 Quizz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
